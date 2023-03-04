0bb621309d82b5895edc14717ee397557600adb2
itsmiraz <mirajulislamofficial044@gmail.com>
2023-03-04 15:56:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter } from "react-router-dom";
+import About from "./components/About";
+import { StarsCanvas } from "./components/canvas";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Feedbacks from "./components/Feedbacks";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Tech from "./components/Tech";
+import Works from "./components/Works";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="relative bg-primary z-0">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
